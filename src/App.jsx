17a590ccb1afadb74f91a41dee2e8c5405baca5a
Sahil Singh Virdhi <sahilsv003@gmail.com>
2024-03-28 12:01:53 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Expense Tracker</h1>
+      <div className='dashboard'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
